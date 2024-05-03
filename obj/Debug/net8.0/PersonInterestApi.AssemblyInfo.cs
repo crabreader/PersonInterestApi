8c89526604c8568b74aabb12cd0690726e351322
Alexander Doja <alexander.doja@outlook.com>
2024-05-03 14:55:16 +0200

    Initial commit

--- obj/Debug/net8.0/PersonInterestApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/PersonInterestApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonInterestApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdc15380f88d5c821a40b89dcacdcc97ec3e4a79")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonInterestApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonInterestApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
